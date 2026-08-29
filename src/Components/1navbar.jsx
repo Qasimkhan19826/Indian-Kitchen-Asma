474ce4130a82d78e8396057c3bf66a7cf82efe82
Qasim Khan <qasimkhan19826@gmail.com>
2026-08-29 20:35:59 +0530

    Tenth

--- src/Components/1navbar.jsx
+++ src/Components/1navbar.jsx
@@ -4,8 +4,6 @@ import Logo from '../assets/Indian_Kitchen_Logo.jpeg';
 import { ShoppingCart,Sun,Moon } from 'lucide-react'; 
  
  
- 
- 
 function Navbar(props) { 
  
 // Creating our poper (Responsive Mobile Navbar) 
@@ -26,6 +24,29 @@ useEffect(() => {
         <div id="logo"> 
             <img className="image" src={Logo} alt="Indian Kitchen Asma "/> 
         </div> 
+
+          {/* ============================= */}
+          {/* By ChatGpt ADD ON: PHONE NAVBAR BUTTONS */}
+          {/* ============================= */}
+
+          <div className="mobile-actions" >
+
+              {/* Cart button for phone */}
+              <button className="btn6">
+                  <ShoppingCart size={28} strokeWidth={2} />
+              </button>
+
+              {/* Dark / Light button for phone */}
+              <button className="btn3 mobile-mode" onClick={props.toggleMode}>
+                  {props.mode === 'light'
+                      ? <Sun size={32} strokeWidth={1} />
+                      : <Moon size={32} strokeWidth={1} />
+                  }
+              </button>
+
+    
+        </div>
+        
         <ul className='desktop-menu'> 
             <li className="item"><a href="#">{props.text[props.language].home}</a></li> 
             <li className="item"><a href="#">{props.text[props.language].services}</a></li> 
@@ -53,13 +74,8 @@ useEffect(() => {
           {props.mode === 'light'?<Sun size={32} strokeWidth={1} /> : <Moon size={32} strokeWidth={1}/> } 
         </button> 
  
-        {/* <button onClick={props.toggleMode}> 
-        {props.mode === "light" ? <Moon size={28} /> : <Sun size={28} />} 
-        </button> 
-  */} 
  
-        {/* <button className="btn6">  
-          <img className="imagemode"   src={props.mode === 'light'?wcart:dcart} style={{backgroundColor:props.mode === 'light'?'white':'black'}}/></button> */} 
+         
         </div> 
  
         {/* Phone Menu Button */} 
@@ -70,21 +86,30 @@ useEffect(() => {
  
         {/* Right Side Mobile Menu */} 
          
-        <div className={`mobile-menu ${menuopen ?"open":""}`} >
+        <div className={`mobile-menu ${menuopen ?"open":""}`} style={{backgroundColor:props.mode === 'light'?'white':'#061625'}} >
  
           {/*  Close Button  */} 
           <button  
-          className="close-button" 
+          className="close-button"  style={{color:props.mode === 'light'?'black':'white'}}
           onClick={()=>setMenuOpen(false)}> 
             x 
           </button> 
  
           {/* Copying Desktop Content only taking anchor tags of ul>li rest of the things are almost same  */} 
-          <a href="#">{props.text[props.language].home}</a> 
-          <a href="#">{props.text[props.language].services}</a> 
-          <a href="#">{props.text[props.language].contact}</a> 
-          <button className="btn4">{props.text[props.language].login}</button> 
-          <button className="btn4">{props.text[props.language].signin}</button> 
+          <a href="#" style={{color:props.mode === 'light'?'black':'white'}} >
+          {props.text[props.language].home}</a> 
+
+          <a href="#"  style={{color:props.mode === 'light'?'black':'white'}}>
+          {props.text[props.language].services}</a> 
+
+          <a href="#"  style={{color:props.mode === 'light'?'black':'white'}}>
+          {props.text[props.language].contact}</a> 
+          
+          <button className="btn4">
+          {props.text[props.language].login}</button> 
+          <button className="btn4">
+          {props.text[props.language].signin}</button> 
+          
  
  
          <select className='btn4' 
